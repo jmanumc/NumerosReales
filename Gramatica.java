a88f392da2e0c8278aa0622ec62e9e4f45bc624d
jmanumc <jmanumc@outlook.com>
2018-04-25 18:34:24 -0500

    Resuelto un problema con el tabulador

--- Gramatica.java
+++ Gramatica.java
@@ -1,5 +1,6 @@
 public class Gramatica {
 	private static final String IGUAL = "=>";
+	private static final String PUNTOS = "::";
 	private final String tabulador;
 	private final String clave;
 	private String valor;
@@ -7,11 +8,11 @@ public class Gramatica {
 	public Gramatica(String clave) {
 		this.clave = clave;
 		this.valor = "";
-		this.tabulador = new String(new char[clave.length()]).replace("\0", " ");
+		this.tabulador = new String(new char[clave.length() + PUNTOS.length()]).replace("\0", " ");
 	}
 
 	public void agregarValorConClave(String valor) {
-		this.valor += clave + IGUAL + valor + "\n";
+		this.valor += clave + PUNTOS + IGUAL + valor + "\n";
 	}
 
 	public void agregarValorSinClave(String valor) {
