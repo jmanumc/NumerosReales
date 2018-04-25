5b5d2a76020b05ede11b563d6acfe9a7c5800d15
jmanumc <jmanumc@outlook.com>
2018-04-25 16:06:51 -0500

    Modificado el submenu de numeros reales con signo

--- NumerosReales.java
+++ NumerosReales.java
@@ -2,16 +2,23 @@ public class NumerosReales {
 	public static String numeroRealConSigno() {
 		String resultado = "<N. Real c/signo>";
 
-		System.out.println("< N. Real c/signo>: \n1. +<N. Real s/signo> \n2. -< N. Real s/signo >");
-		System.out.println("Esperando...\n");
-		int a=Leer.datoInt();
+		System.out.println("\n< N. Real c/signo>:");
+		System.out.println("\n1. +<N. Real s/signo>");
+		System.out.println("2. -< N. Real s/signo >");
+		System.out.print("\nSelecione una opcion: ");
+		int opcion = Leer.datoInt();
 
-		if (a == 1) {
-			String ntcs = "<N. Real c/signo>";
-			System.out.println("< N. Real c/signo>: \n1. +<N. Real s/signo> \n2. -< N. Real s/signo >");	
-		} else {
-			String ntss = "<N. Real s/signo>";
+		switch (opcion) {
+		case 1:
+			resultado = "+" + resultado;
+			System.out.println("< N. Real c/signo>: \n1. +<N. Real s/signo> \n2. -< N. Real s/signo >");
+			break;
+		case 2:
+			resultado = "-" + resultado;
 			System.out.println("< N. Real s/signo>: \n1. <N. Real c/signo> \n2. < N. Real s/signo >");
+			break;
+		default:
+			System.out.println("La opcion elegida es incorrecta :(");
 		}
 
 		return resultado;
