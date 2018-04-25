4846c95ff7156693bc5b813c17cea0b8da4f7463
jmanumc <jmanumc@outlook.com>
2018-04-25 15:45:53 -0500

    Cambiado el menu principal

--- NumerosReales.java
+++ NumerosReales.java
@@ -1,26 +1,36 @@
-public class NumerosReales{
-	
-	public static void main(String [] ar){
-		int a;
-		System.out.println("< N. Real >: \n1.- <N. Real c/signo> \n2.- < N. Real s/signo >");
-		System.out.println("Esperando...\n");
-		a=Leer.datoInt();
-		if(a==1){
+public class NumerosReales {
+	public static void main(String [] args) {
+		int opcion;
+
+		System.out.println("\n< N. Real >:");
+		System.out.println("\n1.- <N. Real c/signo>");
+		System.out.println("2.- < N. Real s/signo >");
+		System.out.print("\nSelecione una opcion: ");
+		opcion = Leer.datoInt();
+		
+		switch (opcion) {
+		case 1:
 			String cs = "<N. Real c/signo>";
 			System.out.println("< N. Real c/signo>: \n1. +<N. Real s/signo> \n2. -< N. Real s/signo >");
 			System.out.println("Esperando...\n");
-			a=Leer.datoInt();
+			int a=Leer.datoInt();
 
-			if(a==1){
+			if (a == 1) {
 				String ntcs = "<N. Real c/signo>";
 				System.out.println("< N. Real c/signo>: \n1. +<N. Real s/signo> \n2. -< N. Real s/signo >");	
-			}else{
+			} else {
 				String ntss = "<N. Real s/signo>";
 				System.out.println("< N. Real s/signo>: \n1. <N. Real c/signo> \n2. < N. Real s/signo >");
-			}	
-		}else{
+			}
+
+			break;
+		case 2:
 			String nss = "<N. Real s/signo>";
 			System.out.println("< N. Real s/signo>: \n1. <N. Real c/signo> \n2. < N. Real s/signo >");
+
+			break;
+		default:
+			System.out.println("La opcion elegida es incorrecta :(");
 		}
 	}
 }
